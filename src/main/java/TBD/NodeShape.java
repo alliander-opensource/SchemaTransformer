d1a645298419e5d6a9d40e787883575d83e5f61a
Rick Suijkerbuijk <r.r.suijkerbuijk@gmail.com>
2021-10-08 15:31:06 +0200

    added functionality -> now able to fully construct nodes inmemory

--- src/main/java/TBD/NodeShape.java
+++ src/main/java/TBD/NodeShape.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public class NodeShape {
 
+    private final String nodeShapeID;
     private final String targetClass;
     private final List<Property> propertyList;
 
-    public NodeShape(String targetClass, List<Property> propertyList){
+    public NodeShape(String nodeShapeID, String targetClass, List<Property> propertyList){
+        this.nodeShapeID = nodeShapeID;
         this.targetClass = targetClass;
         this.propertyList = propertyList;
     }
