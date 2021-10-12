b81963f916abc95c3d96fbdaaece0399edf795e6
Rick Suijkerbuijk <rick.suijkerbuijk@jcore.com>
2021-10-12 13:33:05 +0200

    added rootObject identification

--- src/main/java/TBD/NodeShape.java
+++ src/main/java/TBD/NodeShape.java
@@ -6,12 +6,14 @@ public class NodeShape {
 
     private final String nodeShapeID;
     private final String targetClass;
+    private final boolean rootObject;
     private final List<String> enumeration;
     private final List<Property> propertyList;
 
-    public NodeShape(String nodeShapeID, String targetClass, List<String> enumeration, List<Property> propertyList){
+    public NodeShape(String nodeShapeID, String targetClass, boolean rootObject, List<String> enumeration, List<Property> propertyList){
         this.nodeShapeID = nodeShapeID;
         this.targetClass = targetClass;
+        this.rootObject = rootObject;
         this.enumeration = enumeration;
         this.propertyList = propertyList;
     }
