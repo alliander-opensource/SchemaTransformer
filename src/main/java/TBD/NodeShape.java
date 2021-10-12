a15a383da16384a3097cb91670f1c958c2de9b02
Rick Suijkerbuijk <rick.suijkerbuijk@jcore.com>
2021-10-12 11:27:14 +0200

    enumeration part of a shape is now also being mapped also added maintaining order of shapes and properties

--- src/main/java/TBD/NodeShape.java
+++ src/main/java/TBD/NodeShape.java
@@ -6,11 +6,13 @@ public class NodeShape {
 
     private final String nodeShapeID;
     private final String targetClass;
+    private final List<String> enumeration;
     private final List<Property> propertyList;
 
-    public NodeShape(String nodeShapeID, String targetClass, List<Property> propertyList){
+    public NodeShape(String nodeShapeID, String targetClass, List<String> enumeration, List<Property> propertyList){
         this.nodeShapeID = nodeShapeID;
         this.targetClass = targetClass;
+        this.enumeration = enumeration;
         this.propertyList = propertyList;
     }
 
