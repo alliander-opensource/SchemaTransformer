4f125ab942340f44b45e1d8a532c50666acb847b
Bart Kleijngeld <bartkl@gmail.com>
2022-01-26 15:32:11 +0100

    Refactor of ProfileReader

--- app/src/main/kotlin/schemaTransformer/App.kt
+++ app/src/main/kotlin/schemaTransformer/App.kt
@@ -1,8 +1,13 @@
 package schemaTransformer
 
+import kotlin.io.path.Path
+
+
 fun main(args: Array<String>) {
 //    val profiles = ProfileReader.read("app/src/main/resources/rdfs/ExampleProfile.ttl")
-    val profiles = ProfileReader.read("app/src/main/resources/ExampleProfile.png")
+//    val profiles = ProfileReader.read("app/src/main/resources/ExampleProfile.png")
+    val profiles = ProfileReader.read(Path("app/src/main/resources/ExampleProfile.png"))
 
     println(profiles)
+
 }
--- app/src/main/kotlin/schemaTransformer/ProfileReader.kt
+++ app/src/main/kotlin/schemaTransformer/ProfileReader.kt
@@ -2,7 +2,6 @@ package schemaTransformer
 
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.stream.Stream
 import kotlin.io.path.*
 import kotlin.streams.toList
 import org.eclipse.rdf4j.model.Model
@@ -10,18 +9,18 @@ import org.eclipse.rdf4j.rio.Rio
 
 typealias ProfileFilePath = Path
 
-
 object ProfileReader {
-    fun read(inputPath: String, recursive: Boolean = true): List<Model?> {
-        val path = Path(inputPath)
-        val files: Stream<Path> = if (recursive) Files.walk(path) else Files.walk(path, 1)
+    fun read(path: String, recursive: Boolean = true): List<Model> =
+        read(Path(path), recursive)
+
+    fun read(path: Path, recursive: Boolean = true): List<Model> {
+        val files = if (recursive) Files.walk(path) else Files.walk(path, 1)
 
         return files.filter { it.isRegularFile() }.map { parseFile(it) }.toList().filterNotNull()
     }
 
-    private fun parseFile(filePath: ProfileFilePath): Model? {
-        val rdfFormat = Rio.getParserFormatForFileName(filePath.toString()).unwrap()
-
-        return rdfFormat?.let { Rio.parse(filePath.reader(), "", it) }
-    }
+    private fun parseFile(filePath: ProfileFilePath): Model? =
+        with(Rio.getParserFormatForFileName(filePath.toString()).unwrap()) {
+            this?.let { Rio.parse(filePath.reader(), "", it) }
+        }
 }
