212169c7c2e0700e56c918611f16b100d70c0b09
Bart Kleijngeld <bartkl@gmail.com>
2022-01-26 11:49:04 +0100

    Simple file parsing with Rio (WIP)

--- app/src/main/kotlin/schemaTransformer/ProfileReader.kt
+++ app/src/main/kotlin/schemaTransformer/ProfileReader.kt
@@ -2,29 +2,28 @@ package schemaTransformer
 
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.*
 import java.util.stream.Stream
 import kotlin.io.path.*
 import kotlin.streams.toList
 import org.eclipse.rdf4j.model.Model
-import org.eclipse.rdf4j.rio.RDFFormat
 import org.eclipse.rdf4j.rio.Rio
 
 typealias ProfileFilePath = Path
 
+fun <T> Optional<T>.unwrap(): T? = orElse(null)
+
 object ProfileReader {
-    fun read(inputPath: String, recursive: Boolean = true): List<Model> {
+    fun read(inputPath: String, recursive: Boolean = true): List<Model?> {
         val path = Path(inputPath)
         val files: Stream<Path> = if (recursive) Files.walk(path) else Files.walk(path, 1)
 
-        return files.filter { it.isRegularFile() }.map { readFile(it) }.toList()
+        return files.filter { it.isRegularFile() }.map { parseFile(it) }.toList()
     }
 
-    private fun readFile(filePath: ProfileFilePath): Model {
-        // TODO: Skip non-RDF files.
+    private fun parseFile(filePath: ProfileFilePath): Model? {
+        val rdfFormat = Rio.getParserFormatForFileName(filePath.toString()).unwrap()
 
-        val fileReader = filePath.reader()
-        val f = Rio.parse(fileReader, "",RDFFormat.TURTLE)
-//        fileReader.use { it.forEachLine { print(it) } }
-        return f
+        return rdfFormat?.let { Rio.parse(filePath.reader(), "", it) }
     }
 }
