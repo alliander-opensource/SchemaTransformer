1ad40325ef59ad3d58e20d75a7fd9821e6c5a210
Bart Kleijngeld <bartkl@gmail.com>
2022-01-26 11:02:43 +0100

    Added example resources, and made POC of parsing Turtle RDF file work

--- app/src/main/kotlin/schemaTransformer/App.kt
+++ app/src/main/kotlin/schemaTransformer/App.kt
@@ -1,9 +1,8 @@
 package schemaTransformer
 
 fun main(args: Array<String>) {
-  val profiles = ProfileReader.read("src/main/resources")
-  //    val profiles = read("src/main/resources/testme.txt")
+    val profiles = ProfileReader.read("app/src/main/resources/rdfs/ExampleProfile.ttl")
+    //    val profiles = read("src/main/resources/testme.txt")
 
-  println(profiles)
-  println(ProfileReader.print())
+    println(profiles)
 }
--- app/src/main/kotlin/schemaTransformer/ProfileReader.kt
+++ app/src/main/kotlin/schemaTransformer/ProfileReader.kt
@@ -5,30 +5,26 @@ import java.nio.file.Path
 import java.util.stream.Stream
 import kotlin.io.path.*
 import kotlin.streams.toList
+import org.eclipse.rdf4j.model.Model
 import org.eclipse.rdf4j.rio.RDFFormat
+import org.eclipse.rdf4j.rio.Rio
 
 typealias ProfileFilePath = Path
 
-typealias RdfModel = String
-
 object ProfileReader {
-    fun read(inputPath: String, recursive: Boolean = true): List<RdfModel> {
+    fun read(inputPath: String, recursive: Boolean = true): List<Model> {
         val path = Path(inputPath)
         val files: Stream<Path> = if (recursive) Files.walk(path) else Files.walk(path, 1)
 
         return files.filter { it.isRegularFile() }.map { readFile(it) }.toList()
     }
 
-    fun print() {
-        print(RDFFormat.RDFXML)
-    }
-
-    private fun readFile(filePath: ProfileFilePath): RdfModel {
+    private fun readFile(filePath: ProfileFilePath): Model {
         // TODO: Skip non-RDF files.
 
         val fileReader = filePath.reader()
-        fileReader.use { it.forEachLine { print(it) } }
-
-        return ""
+        val f = Rio.parse(fileReader, "",RDFFormat.TURTLE)
+//        fileReader.use { it.forEachLine { print(it) } }
+        return f
     }
 }
