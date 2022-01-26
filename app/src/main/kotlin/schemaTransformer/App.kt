16b5986ca3218629412db18bdd53fce9ef61e582
Bart Kleijngeld <bartkl@gmail.com>
2022-01-26 19:40:01 +0100

    First meaningful unit test (although MVP)

--- app/src/main/kotlin/schemaTransformer/App.kt
+++ app/src/main/kotlin/schemaTransformer/App.kt
@@ -1,13 +1,13 @@
 package schemaTransformer
 
-import kotlin.io.path.Path
 
-
-fun main(args: Array<String>) {
-    val profiles = ProfileReader.read("app/src/main/resources/rdfs/")
-//    val profiles = ProfileReader.read("app/src/main/resources/rdfs/ExampleProfile.ttl")
-//    val profiles = ProfileReader.read(Path("app/src/main/resources/ExampleProfile.png"))
+fun main() {
+    val profiles = ProfileReader.read("app/src/test/resources/rdfs/")
+//    val profiles = ProfileReader.read("app/src/test/resources/rdfs/ExampleProfile.ttl")
+//    val profiles = ProfileReader.read(Path("app/src/test/resources/ExampleProfile.png"))
 
     println(profiles[1])
+
+
 
 }
--- app/src/test/kotlin/schemaTransformer/ProfileReaderTest.kt
+++ app/src/test/kotlin/schemaTransformer/ProfileReaderTest.kt
@@ -2,9 +2,24 @@ package schemaTransformer
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import org.eclipse.rdf4j.model.Model
+import org.eclipse.rdf4j.rio.RDFFormat
+import kotlin.io.path.Path
+import org.eclipse.rdf4j.rio.Rio
+import java.nio.file.Path
+import kotlin.io.path.reader
+
+
+private fun parseTtlFile(f: Path): Model = Rio.parse(f.reader(), "", RDFFormat.TURTLE)
+
+
+
+val jemoeder = Path("").toAbsolutePath().toString()
+val ttlExample = Path("src/test/resources/rdfs/ExampleProfile.ttl")
 
 class ProfileReaderTest : FunSpec ({
+    println(jemoeder)
     test("Reading ttl file should return RDF model") {
-        "sammy".length shouldBe 5
+        listOf(parseTtlFile(ttlExample)) shouldBe ProfileReader.read(ttlExample)
     }
 })
