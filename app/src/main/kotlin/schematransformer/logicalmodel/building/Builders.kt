92b70a5f20a019d4209107d4f18a4bb33cfd9c2b
Bart Kleijngeld <bartkl@gmail.com>
2022-02-09 19:48:08 +0100

    Cleanup of old code

--- app/src/main/kotlin/schematransformer/logicalmodel/building/Builders.kt
+++ app/src/main/kotlin/schematransformer/logicalmodel/building/Builders.kt
@@ -1,22 +1,24 @@
 package schematransformer.logicalmodel.building
 
 import org.eclipse.rdf4j.model.Model
+import schematransformer.logicalmodel.LogicalModel
+import schematransformer.logicalmodel.ProfileModel
+import schematransformer.logicalmodel.RdfType
+import schematransformer.logicalmodel.type
 
 
-//private fun buildLogicalModels(rdfModels: Sequence<Model>, ): List<LogicalModel> {
-//    val modelsByType = rdfModels.groupBy { it.type }
-//    val profiles = modelsByType[RdfType.Profile()]
-//
-//    return profiles.map {}
+private fun buildLogicalModels(rdfModels: List<Model>): List<LogicalModel>? {
+    val modelsByType = rdfModels.groupBy { it.type }
+    val profiles = modelsByType[RdfType.Profile()] ?: return null
+
+    return profiles.map { buildLogicalModel(it, modelsByType) }
+}
 
 
-
-//}
-
-//private fun buildLogicalModel(
-//    profile: ProfileModel,
-//    modelsByType: Map<RdfType, Model>
-//): LogicalModel {
-//    TODO()
-//}
+private fun buildLogicalModel(
+    profile: ProfileModel,
+    associatedModelsByType: Map<RdfType, List<Model>>
+): LogicalModel {
+    TODO()
+}
 
