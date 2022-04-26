828a175f335369a3606a00d64ab63f1dd90d24da
Bart Kleijngeld <bartkl@gmail.com>
2022-04-26 19:22:57 +0200

    Cleanup

--- app/src/main/kotlin/schematransformer/read/Read.kt
+++ app/src/main/kotlin/schematransformer/read/Read.kt
@@ -4,16 +4,10 @@ import java.io.File
 import org.eclipse.rdf4j.model.IRI
 import org.eclipse.rdf4j.model.Model
 import org.eclipse.rdf4j.model.impl.TreeModel
-import org.eclipse.rdf4j.model.util.Values
-import org.eclipse.rdf4j.model.vocabulary.RDF
 import org.eclipse.rdf4j.rio.Rio
 import org.eclipse.rdf4j.rio.UnsupportedRDFormatException
-import schematransformer.vocabulary.DXPROFILE
 
 val SUPPORTED_FILE_EXTENSIONS = listOf("ttl", "xml", "jsonld")
-
-fun generateContextFromFileName(nsPrefix: String = "", file: File): IRI =
-    Values.iri("${nsPrefix}:${file.name}")
 
 fun readFile(file: File, contextFn: (file: File) -> IRI): Model {
     val format =
