95364240a6734250e69192a7daf5334cecaf7b8d
Bart Kleijngeld <bartkl@gmail.com>
2022-04-27 09:37:44 +0200

    Cleanup

--- app/src/main/kotlin/schematransformer/DxProf.kt
+++ app/src/main/kotlin/schematransformer/DxProf.kt
@@ -10,18 +10,3 @@ import schematransformer.vocabulary.DXPROFILE
 fun Model.getProfileIRIs(): Set<Resource> = this.filter(null, RDF.TYPE, DXPROFILE.PROFILE).subjects()
 fun Model.getResourceStatements(profileIri: IRI): List<Statement> =
     this.filter { it.subject in this.filter(profileIri, DXPROFILE.HASRESOURCE, null).objects() }
-
-
-//fun assignSchemaNamedGraphs(model: Model): Any {
-//    val profileIRIs =
-//        model.filter { it.predicate == RDF.TYPE && it.`object` == DXPROFILE.PROFILE }.map {
-//            it.subject
-//        }
-//
-//    for (profileIri in profileIRIs) {
-//        val resources = getResourceStatements(model, profileIri as IRI)
-//        println()
-//    }
-//
-//    return 1
-//}
