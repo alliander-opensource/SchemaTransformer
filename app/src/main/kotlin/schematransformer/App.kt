84efbefc5cc53110bc9a0635470eafbb6111c3e9
Bart Kleijngeld <bartkl@gmail.com>
2022-04-27 08:48:47 +0200

    Better filtering on models

--- app/src/main/kotlin/schematransformer/App.kt
+++ app/src/main/kotlin/schematransformer/App.kt
@@ -1,12 +1,17 @@
 package schematransformer
 
+import org.eclipse.rdf4j.model.IRI
 import org.eclipse.rdf4j.model.util.Values
 import java.io.File
 import schematransformer.read.readDirectory
 
 fun main() {
     val directory = File("app/src/test/resources/rdfs")
-    val model = readDirectory(directory, contextFn = { path -> Values.iri("iyrtpc:${path.name}") })
+    val model = readDirectory(directory, contextFn = { path -> Values.iri("file://${path.absolutePath}") })
 
+    println(model)
+    val p = model.getProfileIRIs().first()
+    val rs = model.getResourceStatements(p as IRI)
+    println(rs)
 
 }
--- app/src/main/kotlin/schematransformer/DxProf.kt
+++ app/src/main/kotlin/schematransformer/DxProf.kt
@@ -2,29 +2,33 @@ package schematransformer
 
 import org.eclipse.rdf4j.model.IRI
 import org.eclipse.rdf4j.model.Model
+import org.eclipse.rdf4j.model.Resource
 import org.eclipse.rdf4j.model.Statement
 import org.eclipse.rdf4j.model.vocabulary.RDF
 import schematransformer.vocabulary.DXPROFILE
 
-private fun getResources(model: Model, profileIri: IRI): List<Statement> {
-    val resourceIRIs =
-        model
-            .filter { it.subject == profileIri && it.predicate == DXPROFILE.HASRESOURCE }
-            .map { it.`object` }
 
-    return model.filter { st -> st.subject in resourceIRIs }
+//fun Model.
+
+fun Model.getProfileIRIs(): Set<Resource> = this.filter(null, RDF.TYPE, DXPROFILE.PROFILE).subjects()
+fun Model.getResourceStatementss(profileIri: IRI): List<Statement> {
+    val resourceIRIs = this.filter(null, DXPROFILE.HASRESOURCE, null).objects()
+    return this.filter { it.subject in resourceIRIs }
 }
+fun Model.getResourceStatements(profileIri: IRI): List<Statement> =
+    this.filter { it.subject in this.filter(null, DXPROFILE.HASRESOURCE, null).objects() }
 
-fun assignSchemaNamedGraphs(model: Model): Any {
-    val profileIRIs =
-        model.filter { it.predicate == RDF.TYPE && it.`object` == DXPROFILE.PROFILE }.map {
-            it.subject
-        }
 
-    for (profileIri in profileIRIs) {
-        val resources = getResources(model, profileIri as IRI)
-        println()
-    }
-
-    return 1
-}
+//fun assignSchemaNamedGraphs(model: Model): Any {
+//    val profileIRIs =
+//        model.filter { it.predicate == RDF.TYPE && it.`object` == DXPROFILE.PROFILE }.map {
+//            it.subject
+//        }
+//
+//    for (profileIri in profileIRIs) {
+//        val resources = getResourceStatements(model, profileIri as IRI)
+//        println()
+//    }
+//
+//    return 1
+//}
