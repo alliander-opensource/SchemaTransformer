8434692cf96d3eacf4562ed03e224e8af6626eda
Bart Kleijngeld <bartkl@gmail.com>
2022-04-28 09:33:48 +0200

    Schema building setup

--- app/src/main/kotlin/schematransformer/AvroPOC.kt
+++ app/src/main/kotlin/schematransformer/AvroPOC.kt
@@ -1,6 +1,15 @@
 package schematransformer
 
+import org.apache.avro.Schema
 import org.apache.avro.SchemaBuilder
+import org.eclipse.rdf4j.model.IRI
+import org.eclipse.rdf4j.model.Model
+
+
+fun buildSchema(model: Model, rootNodeShape: IRI): Schema {
+
+    TODO()
+}
 
 fun main() {
     val D = SchemaBuilder.record("D")
@@ -19,6 +28,12 @@ fun main() {
         .name("FromBtoD").type(FromBtoDType).noDefault()
         .name("FromBtoDButSomehowDifferent").type(D).noDefault()
         .endRecord()
+
+    val f = Schema.Field("BtoD", FromBtoDType)
+    println(f)
+
+    // TODO: Fix dat de docs worden opgepikt in de fields.
+    // Kan blijkbaar niet met de builder? Rick doet het achteraf met een map.
 
     println(B)
 }
