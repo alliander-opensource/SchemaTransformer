e673efb55ada066f7030790a26632c727f21de5b
Bart Kleijngeld <bartkl@gmail.com>
2022-06-02 15:09:19 +0200

    Cleanup

--- app/src/main/kotlin/schematransformer/Exceptions.kt
+++ app/src/main/kotlin/schematransformer/Exceptions.kt
@@ -5,14 +5,14 @@ open class SchemaTransformerException(message: String) : Exception(message)
 class NoRootObjectException(message: String = "No root object found.") : SchemaTransformerException(message)
 
 class UnsupportedCardinalityException(
-    message: String = "Unsupported `minCount` and `maxCount` for determining cardinality."
+    message: String = "Unsupported `sh:minCount` and `sh:maxCount` for determining cardinality."
 ) : SchemaTransformerException(message)
 
 class EnumSymbolsNotFoundException(
-    message: String = "No symbols found for enum."
+    message: String = "No symbols found for enumeration schema."
 ) : SchemaTransformerException(message)
 
 class IncompatiblePropertyShapeException(
-    message: String = "Property shape must contain either sh:node or sh:datatype."
+    message: String = "Property shape must contain either `sh:node` or `sh:datatype`."
 ) :
     SchemaTransformerException(message)
